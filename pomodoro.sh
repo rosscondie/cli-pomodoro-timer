054df62d18437512d4561e0e8e0896bdd5ff83fa
Ross Condie <rosscondie@gmail.com>
2025-06-04 11:19:17 +0200

    Update script so \$SCRIPT_DIR resolves when being sourced with zsh

--- pomodoro.sh
+++ pomodoro.sh
@@ -5,14 +5,21 @@ pomo_options["work"]="45"
 pomo_options["break"]="10"
 
 # Update this path to your actual sound file (wav recommended if using aplay)
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+if [[ -n "${BASH_SOURCE[0]}" ]]; then
+    SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+else
+    SCRIPT_DIR="$(cd "$(dirname "${(%):-%N}")" && pwd)"  # for zsh
+fi
+
 SOUND_FILE="$SCRIPT_DIR/alert.wav"
 
 play_sound() {
     if command -v afplay &>/dev/null; then
         afplay "$SOUND_FILE" &>/dev/null
+        sleep 0.5
     elif command -v aplay &>/dev/null; then 
         aplay "$SOUND_FILE" &>/dev/null; 
+        sleep 0.5
     else
         echo "sound player not found, cannot play sound."
     fi
